b32c944f987b7f323243ed2885d2373e32db0e53
Linus <linus@mimietz.de>
2022-12-28 01:06:41 +0100

    Bug fix for Windows paths

--- script.py
+++ script.py
@@ -2,16 +2,16 @@ from io import BytesIO
 from itertools import repeat
 from wand.image import Image
 from PIL import Image as PILImage
-import os, re, numpy, requests, replicate, multiprocessing, tqdm
+import os, re, numpy, requests, replicate, multiprocessing, tqdm, platform
 threadCount = 4
-delimiter = os.path.sep
+delimiter = "\\" if platform.system() == "Windows" else "/"
 
 def upscaleImage(args):
     imagePath, colorization = args
-    if not os.path.isfile(imagePath) or not re.search(r'\.(png|jpg|jpeg)$', imagePath):
+    if not os.path.isfile(imagePath) or not re.search(r'.(png|jpg|jpeg)$', imagePath):
         print("Error: File does not exist or is not a JPG/PNG image: " + imagePath)
         exit()
-    imagePathWithoutExtension = re.sub(r'\.(png|jpg|jpeg)$', "", imagePath)
+    imagePathWithoutExtension = re.sub(r'.(png|jpg|jpeg)$', "", imagePath)
     tempFolderName = "temp-" + str(numpy.random.randint(1000000000))
     os.makedirs(tempFolderName)
     
@@ -48,7 +48,10 @@ def upscaleImage(args):
     model = replicate.models.get("nightmareai/real-esrgan")
     version = model.versions.get("42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b")
     imageUrl = version.predict(image=open(tempFolderName + delimiter + "low-res.png", "rb"),scale=4,face_enhance=False)
-    os.system("rm -rf " + tempFolderName)
+    if platform.system() == "Windows":
+        os.system("rmdir /s /q " + tempFolderName)
+    else:
+        os.system("rm -rf " + tempFolderName)
     # download and save the image
     response = requests.get(imageUrl)
     if response.status_code != 200:
@@ -75,8 +78,8 @@ if __name__ == "__main__":
     colorizationInput = input("Colorize images? (y/n): ").lower()
     colorization = True if colorizationInput == "y" or colorizationInput == "yes" else False
     files = [folderPath + delimiter + f for f in os.listdir(folderPath) if os.path.isfile(os.path.join(folderPath, f))]
-    files = [f for f in files if re.search(r'\.(png|jpg|jpeg)$', f, re.IGNORECASE)]
-    files = [f for f in files if not re.search(r'(_upscaled_colorized|_upscaled)\.jpeg$', f, re.IGNORECASE)]
+    files = [f for f in files if re.search(r'.(png|jpg|jpeg)$', f, re.IGNORECASE)]
+    files = [f for f in files if not re.search(r'(_upscaled_colorized|_upscaled).jpeg$', f, re.IGNORECASE)]
     if len(files) == 0:
         print("Error: No JPG/PNG images found in folder")
         exit()
